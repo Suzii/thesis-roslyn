86942ddc622cc424058f2c76b972702b34d5e5dd
Suzii <z.dankovcikova@gmail.com>
2016-10-01 16:38:43 +0200

    Added SampleTestProject for live testing of the analyzer

---
+++ src/SampleTestProject/SampleTestProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
