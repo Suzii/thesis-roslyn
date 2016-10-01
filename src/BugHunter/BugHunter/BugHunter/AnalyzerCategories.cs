cad93fd1b6aa74a029249f2c6a49f3e77acddd9f
Suzii <z.dankovcikova@gmail.com>
2016-10-01 16:36:06 +0200

    Added initial project structure

---
+++ src/BugHunter/BugHunter/BugHunter/AnalyzerCategories.cs
@@ -0,0 +1,43 @@
+﻿namespace BugHunter
+{
+    /// <summary>
+    /// Class defining the analyzer category constants.
+    /// </summary>
+    internal static class AnalyzerCategories
+    {
+        /// <summary>
+        /// Category for ASPX rules
+        /// </summary>
+        internal const string AspxRules = "BugHunter.AspxRules";
+
+        /// <summary>
+        /// Category for control rules
+        /// </summary>
+        internal const string ControlRules = "BugHunter.ControlRules";
+
+        /// <summary>
+        /// Category for CS rules
+        /// </summary>
+        internal const string CsRules = "BugHunter.CsRules";
+
+        /// <summary>
+        /// Category for page rules
+        /// </summary>
+        internal const string PageRules = "BugHunter.PageRules";
+
+        /// <summary>
+        /// Category for project rules
+        /// </summary>
+        internal const string ProjectRules = "BugHunter.ProjectRules";
+
+        /// <summary>
+        /// Category for web config rules
+        /// </summary>
+        internal const string WebConfigRules = "BugHunter.WebConfigRules";
+
+        /// <summary>
+        /// Category for web part rules
+        /// </summary>
+        internal const string WpRules = "BugHunter.WPRules";
+    }
+}
