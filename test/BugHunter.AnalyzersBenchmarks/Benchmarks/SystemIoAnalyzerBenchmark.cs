63ec91f4d3890e45068e6800ca33e4445a5d985b
Suzii <z.dankovcikova@gmail.com>
2017-02-14 13:47:45 +0100

    Add first dummy benchmark for SystemIo

---
+++ test/BugHunter.AnalyzersBenchmarks/Benchmarks/SystemIoAnalyzerBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Exporters;
+using BugHunter.Analyzers.SystemIoRules.Analyzers;
+using BugHunter.TestUtils.Services;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace BugHunter.AnalyzersBenchmarks.Benchmarks
+{
+    [HtmlExporter]
+    [MemoryDiagnoser]
+    public class SystemIoAnalyzerBenchmark
+    {
+        private readonly DiagnosticAnalyzer systemIoV1 = new SystemIOAnalyzer();
+        // TODO
+        private readonly DiagnosticAnalyzer systemIoV2 = new SystemIOAnalyzer();
+
+
+        private readonly Document[] _documentsWithNoDiagnostic;
+        private readonly Document[] _documentsWith1000Diagnostics;
+
+        public SystemIoAnalyzerBenchmark()
+        {
+            var sourceWithNoDiagnostic = File.ReadAllText(@"C:\Users\zuzanad\code\thesis\thesis-sample-test-project\SampleProject\ConsoleApp\SystemIo.cs");
+            var sourceWith1000Diagnostics = File.ReadAllText(@"C:\Users\zuzanad\code\thesis\thesis-sample-test-project\SampleProject\ConsoleApp\StringIndexOfMethod.cs");
+
+            _documentsWithNoDiagnostic = ProjectCompilation.GetDocuments(new[]{ sourceWithNoDiagnostic }, null, null);
+            _documentsWith1000Diagnostics = ProjectCompilation.GetDocuments(new[]{ sourceWith1000Diagnostics }, null, null);
+        }
+
+
+        [Benchmark]
+        public void V1NoDiagnostic()
+        {
+            AnalyzerExecution.GetSortedDiagnosticsFromDocuments(systemIoV1, _documentsWithNoDiagnostic);
+        }
+
+        [Benchmark]
+        public void V11000Diagnostic()
+        {
+            AnalyzerExecution.GetSortedDiagnosticsFromDocuments(systemIoV1, _documentsWith1000Diagnostics);
+        }
+    }
+}
---
+++ test/BugHunter.AnalyzersBenchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using BugHunter.AnalyzersBenchmarks.Benchmarks;
+
+namespace BugHunter.AnalyzersBenchmarks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SystemIoAnalyzerBenchmark>(new ManualConfig() {KeepBenchmarkFiles = true});
+            
+            Console.WriteLine("Benchmarks executed. Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
